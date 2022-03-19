8f133a1c542f4cff8c0126b1944487e486ce2f95
vaishnavivhora <86514711+VaishnaviVhora10@users.noreply.github.com>
2022-03-19 14:32:07 +0530

    learned operators in js

---
+++ bodyMass.js
@@ -0,0 +1,16 @@
+let BMIJhon;
+let BMIMark;
+
+let MarkWeight = 78;
+let MarkHeight = 1.69;
+
+let JhonWeight = 92;
+let JhonHeight = 1.95;
+
+BMIJhon = JhonWeight / (JhonHeight * JhonHeight);
+BMIMark = MarkWeight / (MarkHeight * MarkHeight);
+
+console.log(BMIJhon);
+console.log(BMIMark);
+
+console.log( BMIJhon > BMIMark);
